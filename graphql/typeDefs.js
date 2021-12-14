bf3178e388559181f413af41244a42826a93a1b9
thristov <thristov@abrites.com>
2021-12-14 17:57:46 +0200

    Comment and like posts,comennts and likes count

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -2,22 +2,37 @@ const { gql } = require('apollo-server')
 
 module.exports = gql`
     type Post {
-        id:  ID!,
-        body: String!,
-        createdAt: String!,
+        id:  ID!
+        body: String!
+        createdAt: String!
+        username: String!
+        comments: [Comment]!
+        likes: [Like]!
+        likeCount: Int!
+        commentCount: Int!
+    }
+    type Comment{
+        id: ID!
+        createdAt: String!
+        username: String!
+        body: String!
+    }
+    type Like{
+        id: ID!
+        createdAt: String!
         username: String!
     }
     type User{
-        id: ID!,
-        email: String!,
-        token: String!,
-        username: String!,
+        id: ID!
+        email: String!
+        token: String!
+        username: String!
         createdAt: String!
     }
     input RegisterInput{
-        username: String!,
-        password: String!,
-        confirmPassword: String!,
+        username: String!
+        password: String!
+        confirmPassword: String!
         email: String!
     }
     type Query{
@@ -29,5 +44,8 @@ module.exports = gql`
         login(username: String!, password: String!): User!
         createPost(body:String!): Post!
         deletePost(postId: ID!): String!
+        createComment(postId: String!, body: String!): Post!
+        deleteComment(postId: ID!, commentId: ID!): Post!
+        likePost(postId: ID!): Post!
     }
 `
