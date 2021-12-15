87d2bb837f6a78c3661d85e96cf1631eed6fbb1c
thristov <thristov@abrites.com>
2021-12-15 13:32:52 +0200

    Added Home, Register and Login pages to the client

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import './App.css';
 
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
 function App() {
-  return (
-   <div>
-     <h1>Hi world</h1>
-   </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route exact path='/' element={<Home/>} />   
+                <Route exact path='/login' element={<Login/>} />
+                <Route exact path='/register' element={<Register/>} />   
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
